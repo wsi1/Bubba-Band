93478418e25d2147630fb8df01f1fa8b15fa62b1
derryiri <derryiri@umich.edu>
2021-10-03 17:31:02 -0400

    FIXED hello world

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from 'react';
 
 class App extends Component {
@@ -15,30 +14,6 @@ class App extends Component {
       </div>
     );
   }
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 0bf79f93e1c25fb487a2249d00fb68727df65733
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './App';
 
 ReactDOM.render(
@@ -9,20 +8,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
   );
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 0bf79f93e1c25fb487a2249d00fb68727df65733
