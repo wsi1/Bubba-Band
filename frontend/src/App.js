e26bcb67e6f3cfdc57c0c89478869e14d8ce56a3
derryiri <derryiri@umich.edu>
2021-10-03 17:02:09 -0400

    heLLo wOrLD

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  
+  
+  render () {
+    return (
+      <div className="App">
+          <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
