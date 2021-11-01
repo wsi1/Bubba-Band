2dbd3da90b9d8265fd7fb2c7af6c38f67f22a160
kcamsanc <kcamsanc@umich.edu>
2021-11-01 13:20:12 -0400

    more audios lmfao

--- frontend/src/Calibration/Calibration.js
+++ frontend/src/Calibration/Calibration.js
@@ -1,14 +1,39 @@
+// necessary imports and style
 import React, { Component, useState, useContext, useEffect, useRef } from 'react';
 import { SocketContext } from '../context/socket';
 import { useHistory } from 'react-router-dom';
 import AddGesture from './AddGesture';
-import useSound from "use-sound";
-
-import waiting from "../images/messages-typing.gif";
-import back from "../audios/go_back.mp3";
-import addGesture from "../audios/add_gesture.mp3";
-
 import "./Calibration.css";
+
+// animations
+import waiting from "../images/messages-typing.gif";
+
+// audios
+import back_audio from "../audios/go_back.mp3";
+import add_gesture_audio from  "../audios/add_gesture.mp3";
+import calibration_audio from "../audios/calibration.mp3";
+import waiting_audio from "../audios/waiting.mp3";
+import skip_audio from "../audios/skip.mp3";
+
+let backAudio = new Audio(back_audio);
+let addGestureAudio = new Audio(add_gesture_audio);
+let calibrationAudio = new Audio(calibration_audio);
+let waitingAudio = new Audio(waiting_audio);
+let skipAudio = new Audio(skip_audio);
+
+let allAudios = [backAudio, addGestureAudio, calibrationAudio, waitingAudio, skipAudio];
+
+function playAudio(audio) {
+    // if the mouse moves to another element before the previous sound finishes,
+    // stop the previous sound before playing the new sound
+    // this is to prevent sounds from overlapping one another with quick mouse movement
+    allAudios.forEach(function(a) {
+      a.pause();
+      a.currentTime = 0;
+    })
+  
+    audio.play();
+}
 
 function changeView(nextState, curState, setState, uuid) {
     setState({
@@ -19,7 +44,7 @@ function changeView(nextState, curState, setState, uuid) {
 }
 
 function handleBackPress(setState) {
-    if (window.confirm("You are about to forget the gesture just made, are you sure?")) {
+    if (window.confirm("You are about to skip selecting a label for this gesture. Are you sure?")) {
         setState({
             view: "waiting"
         })
@@ -55,9 +80,6 @@ const Calibration = (props) => {
         uuid: state.uuid
     };
 
-    const [playBack] = useSound(back);
-    const [playAdd] = useSound(addGesture);
-
     const socket = useContext(SocketContext);
     let currGesture = "";
     let numCurrGesture = 0;
@@ -90,15 +112,20 @@ const Calibration = (props) => {
                 <div className="waiting">
                     <button
                         className="goBackButton"
-                        onMouseEnter={() => playBack()}
+                        onMouseEnter={() => playAudio(backAudio)}
                         onClick={() => history.push("/")}>
                         ← Go back
                     </button>
 
-                    <h1>Calibration</h1>
+                    <h1
+                        onMouseEnter={() => playAudio(calibrationAudio)}>
+                        Calibration
+                    </h1>
                     <div className="calibrationContainer">
                         <img src={waiting} alt="Logo" />
-                        <h1 className="smallHead"> Waiting for a gesture to be made ... </h1>
+                        <p onMouseEnter={() => playAudio(waitingAudio)}
+                            > Waiting for a gesture to be made ... 
+                        </p>
                     </div>
                     <button type="button" className="temp" onClick={() => changeView("calibrate", state, setState, "18ae3de03aa34ef686c61029f868f0ceTEST")}> Make a gesture </button>
                 </div>
@@ -107,7 +134,7 @@ const Calibration = (props) => {
                     <div>
                         <button
                             className="goBackButton" id="skip"
-                            onMouseEnter={() => playBack()}
+                            onMouseEnter={() => playAudio(skipAudio)}
                             onClick={() => handleBackPress(setState)}>
                             ← Skip gesture label
                         </button>
@@ -117,7 +144,7 @@ const Calibration = (props) => {
                         <div class="buttons">
                             <button
                                 className="add"
-                                onMouseEnter={() => playAdd()}
+                                onMouseEnter={() => playAudio(addGestureAudio)}
                                 onClick={() => { changeView("add", state, setState, state.uuid) }}>
                                 Add Gesture
                             </button>
--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -2,5 +2,5 @@ import { createContext } from "react";
 import { io } from "socket.io-client";
 
 // open socket to listen
-export const socket = io("http://15c3-35-2-150-10.ngrok.io");
+export const socket = io("http://a5a5-35-3-37-142.ngrok.io");
 export const SocketContext = createContext();
